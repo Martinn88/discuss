0b3a8e64cbf2b6551030629a07ae02e8041f2c1d
Martinn88 <marco_neo@poczta.fm>
2021-06-01 11:02:40 +0200

    Init repo (s9)

---
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,48 @@
+defmodule DiscussWeb.TopicController do
+  use DiscussWeb, :controller
+
+  alias Discuss.Blog
+  alias Discuss.Blog.Topic
+
+  action_fallback DiscussWeb.FallbackController
+
+  def index(conn, _params) do
+    topics = Blog.list_topics()
+    render(conn, "index.json", topics: topics)
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic_params}) do
+    with {:ok, %Topic{} = topic} <- Blog.create_topic(topic_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", Routes.topic_path(conn, :show, topic))
+      |> render("show.json", topic: topic)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    topic = Blog.get_topic!(id)
+    render(conn, "show.json", topic: topic)
+  end
+
+  def update(conn, %{"id" => id, "topic" => topic_params}) do
+    topic = Blog.get_topic!(id)
+
+    with {:ok, %Topic{} = topic} <- Blog.update_topic(topic, topic_params) do
+      render(conn, "show.json", topic: topic)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    topic = Blog.get_topic!(id)
+
+    with {:ok, %Topic{}} <- Blog.delete_topic(topic) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
