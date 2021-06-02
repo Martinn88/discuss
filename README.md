25332ba05861accd4fe1c58e515c273c51f11027
Martinn88 <marco_neo@poczta.fm>
2021-06-02 13:43:27 +0200

    OAuth with GitHub (s11)

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-## Discuss project on Phoenix
+## Discuss project on Phoenix from "The Complete Elixir and Phoenix Bootcamp"
 
 Component versions:
 > Phoenix: 1.5.9,
 > Ecto: 3.6.1,
 > Postgrex: 0.15.9
+> ueberauth: 0.6
+> ueberauth_github: 0.7
+
 
---
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -0,0 +1,35 @@
+defmodule DiscussWeb.AuthController do
+    use DiscussWeb, :controller
+    plug Ueberauth
+
+    alias Discuss.Auth
+
+    action_fallback DiscussWeb.FallbackController
+
+    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+        user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+
+        signin(conn, user_params)
+    end
+
+    defp signin(conn, user_params) do 
+        case insert_or_update_user(user_params) do
+            {:ok, user} ->
+                conn
+                |> put_flash(:info, "Welcom back!")
+                |> put_session(:user_id, user.id)
+                |> redirect(to: Routes.topic_path(conn, :index))
+            {:error, _reason} -> 
+                conn
+                |> put_flash(:error, "Error signing in")
+                |> redirect(to: Routes.topic_path(conn, :index))
+        end
+    end
+
+    defp insert_or_update_user(user_params) do
+        case Auth.get_user_by_email(email: user_params.email) do
+            nil -> Auth.create_user(user_params)
+            user -> {:ok, user}
+        end
+    end
+end
